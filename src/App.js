dde8f0d0fadb7f25fdbe80bb16a5d580beacb788
vicky <vicky906k@gmail.com>
2024-07-19 15:44:30 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import MiddleSection from './components/MiddleSection';
 import Second from './components/Second';
 import SecondMiddle from './components/SecondMiddle';
 import Task from './components/Task';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+    <Header/>
     <MiddleSection/>
     <Second/>
     <SecondMiddle/>
